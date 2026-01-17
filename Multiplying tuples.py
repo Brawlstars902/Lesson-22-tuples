7e24776c7006c4fe478fc616d67ff4658e4ca930
Brawlstars902 <aparna.rautela@gmail.com>
2026-01-17 06:50:49 +0530

    Add files via upload

---
+++ Multiplying tuples.py
@@ -0,0 +1,4 @@
+Tuple1=(1,3,6,9,10,8,4,5,45,23,78,96)
+product=(Tuple1[0]*Tuple1[1]*Tuple1[2]*Tuple1[3]*Tuple1[4]*Tuple1[5]*Tuple1[6]*Tuple1[7]*Tuple1[8]*Tuple1[9]*Tuple1[10]*Tuple1[11])
+
+print(product)
